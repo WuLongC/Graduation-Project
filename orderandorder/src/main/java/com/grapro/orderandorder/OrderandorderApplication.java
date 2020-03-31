fe0d7b3a30d2b4212655b13cb423ef48154be216
WuLongC <1824019720@q.com>
2020-03-31 21:43:14 +0800

    新建项目

---
+++ orderandorder/src/main/java/com/grapro/orderandorder/OrderandorderApplication.java
@@ -0,0 +1,13 @@
+package com.grapro.orderandorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderandorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderandorderApplication.class, args);
+    }
+
+}
